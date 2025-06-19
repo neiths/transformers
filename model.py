94ce3d7477f722be0e00155fd92b5f8108287534
neiths <thienkute01022002@gmail.com>
2025-06-20 00:02:25 +0700

    add position encoding

--- model.py
+++ model.py
@@ -8,23 +8,40 @@ class InputEmbedding(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model) 
         
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
+    
+    
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
         
-
-if __name__ == "__main__":
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0 ,seq_len, dtype=torch.float).unsqueeze(1)
+        
+        # exp(ln()) ~ nothing here, minus stands for not using fraction form.
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10_000.0) / d_model)
+        )
+        
+        pe[:, 0::2] = torch.sin(position*div_term)
+        pe[:, 1::2] = torch.cos(position*div_term)
+        
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
     
-    d_model = 3
-    
-    vocab_size = 10
-    
-    input_embedding = InputEmbedding(d_model, vocab_size)
-    
-    x = torch.LongTensor([1, 2, 3, 4, 5])
-    
-    output = input_embedding(x)
-    
-    print(output) 
     
