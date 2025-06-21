5f34920273fa3ef39abd6848a8142668da6124e0
neiths <thienkute01022002@gmail.com>
2025-06-21 17:48:40 +0700

    add multihead attention layer

--- model.py
+++ model.py
@@ -76,6 +76,55 @@ class FeedForwardBlock(nn.Module):
         return x
 
 
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.attention_scores = None
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+
+        assert d_model % h == 0, "d_model must be divisible by h"
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)  # Wq
+        self.w_k = nn.Linear(d_model, d_model)  # Wk
+        self.w_v = nn.Linear(d_model, d_model)  # Wv
+        self.w_o = nn.Linear(d_model, d_model)  # Wo
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.size(-1)
+
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, float('-inf'))
+
+        attention_scores = attention_scores.softmax(dim=-1)  # (Batch, h, Seq_len, Seq_len)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores  # (Batch, h, Seq_len, d_k)
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        key = self.w_k(k)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+        value = self.w_v(v)  # (Batch, Seq_len, d_model) -> (Batch, Seq_len, d_model)
+
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, h, d_k) --> (Batch, h, Seq_len, d_k)
+        query = query.view(query.size(0), query.size(1), self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.size(0), key.size(1), self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.size(0), value.size(1), self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        #  (Batch, h, Seq_len, d_k) --> (Batch, Seq_len, h, d_k) --> (Batch, Seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.size(0), -1, self.h * self.d_k)
+
+        return self.w_o(x)  # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_model)
+
 
 if __name__ == "__main__":
     d_model = 512
@@ -85,19 +134,18 @@ if __name__ == "__main__":
     input_embedding = InputEmbedding(d_model, vocab_size)
     positional_encoding = PositionalEncoding(d_model, seq_len, dropout=0.1)
     layer_norm = LayerNormlization()
+    ff_block = FeedForwardBlock(d_model, d_ff=2048, dropout=0.1)
+    multi_head_attention = MultiHeadAttention(d_model, h=8, dropout=0.1)
 
-    # Example usage
+    #  Example usage
     x = torch.randint(0, vocab_size, (10, seq_len))  # Batch of 10 sequences
     embedded_x = input_embedding(x)
     encoded_x = positional_encoding(embedded_x)
-    ff_block = FeedForwardBlock(d_model, d_ff=2048, dropout=0.1)
 
-    print(encoded_x.shape)  # Should be (10, seq_len, d_model)
+    # print(encoded_x.shape)  # Should be (10, seq_len, d_model)
 
     normalized_x = layer_norm(encoded_x)
 
-    print(ff_block(normalized_x))
+    # print(ff_block(normalized_x))
 
-
-
-
+    print(multi_head_attention(normalized_x, normalized_x, normalized_x, None))  # Should be (10, seq_len, d_model)
