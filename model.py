ba3e98cfd964b1dc619784f366ad81230c5c3d19
neiths <thienkute01022002@gmail.com>
2025-06-22 14:55:39 +0700

    combine all the blocks into transformer

--- model.py
+++ model.py
@@ -206,6 +206,55 @@ class Decoder(nn.Module):
         return self.norm(x)
 
 
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        #  (Batch, Seq_len, d_model) --> (Batch, Seq_len, vocab_size)
+        return torch.log_softmax(self.proj(x), dim=-1)
+
+
+class Transformer(nn.Module):
+
+    def __init__(
+            self,
+            encoder: Encoder,
+            decoder: Decoder,
+            src_embed: InputEmbedding,
+            tgt_embed: InputEmbedding,
+            src_pos: PositionalEncoding,
+            tgt_pos: PositionalEncoding,
+            projection_layer: ProjectionLayer
+    ) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+
+    def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+
+    def project(self, x):
+        return self.projection_layer(x)
+
+
+
+
+
 if __name__ == "__main__":
     # === Model Setup ===
     d_model = 8  # Example embedding size (small for easy testing)
