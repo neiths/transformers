a6d9322e64cadc03887525604b6f33761e2639f6
neiths <thienkute01022002@gmail.com>
2025-06-22 14:47:10 +0700

    add decoder block

--- model.py
+++ model.py
@@ -167,6 +167,45 @@ class Encoder(nn.Module):
             x = layer(x, src_mask)
         return self.norm(x)
 
+
+class DecoderBlock(nn.Module):
+
+    def __init__(
+            self,
+            self_attention_block: MultiHeadAttention,
+            cross_attention_block: MultiHeadAttention,
+            feed_forward_block: FeedForwardBlock,
+            dropout: float
+    ):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.dropout = nn.Dropout(dropout)
+        self.residual_connections = nn.ModuleList([
+            ResidualConnection(dropout) for _ in range(3)
+        ])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+
+
 if __name__ == "__main__":
     # === Model Setup ===
     d_model = 8  # Example embedding size (small for easy testing)
